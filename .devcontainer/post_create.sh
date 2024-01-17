d971a347529dd52d21968a7cf9d636bc1b08ad73
adehad <26027314+adehad@users.noreply.github.com>
2024-01-17 18:24:24 +0000

    use lighter Docker image

--- .devcontainer/post_create.sh
+++ .devcontainer/post_create.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
- git config --global --add safe.directory /workspaces/pyside6-boilerplate
+git config --global --add safe.directory /workspaces/pyside6-boilerplate
 
-# For Windows users that have cloned into Windows' partition, we do this so that
-# it doesn't show all the files as "changed" for having different line endings.
-# As we use pre-commit for managing our line endings we do this to tell git we don't care
-git config --global core.autocrlf input
-git add .
+# Install pipx
+python -m pip install pipx
+pipx ensurepath
+
+# Allow path changes
+. ~/.bashrc
 
 # Install pdm and pre-commit
 pipx install pre-commit
 pipx install pdm
 
 pdm install
+
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+echo -e "${RED}You may need to 'Reload Window' in VSCode for it to pick up the updated interpreter${NC}"
